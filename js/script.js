46f9036fc7a5d92e12c0c671ab4fd7db8ac670ab
Yukihide Takahashi <yukihide99@gmail.com>
2021-10-02 08:35:33 +0800

    more stuff

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
+    $("body")
+    .css({ overflow: "hidden"})
 
+    $("#catch")
+    .css({"background-image": "linear-gradient(to bottom, rgb(255, 255, 255), rgb(0, 0, 255, 0.2))"})
+    .delay(5000)
+    .fadeOut(1500)
+
+    $("#line1")
+    .delay(500)
+    .css({opacity: 0, "display": "block"})
+    .animate({opacity:1}, 1500);
+
+    $("#line2")
+    .delay(3000)
+    .css({opacity: 0, "display": "block"})
+    .animate({opacity:1}, 1500);
+
+    $("$main")
+    .delay(7000)
+    .css({ opacity: 0.0, visibility: "visible" })
+    .animate({ opacity: 1.0 }, 1000)
 });
