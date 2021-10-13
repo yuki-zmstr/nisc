30b3dcf81ab67e21e09d54898ce46395d461a244
Yukihide Takahashi <yukihide99@gmail.com>
2021-10-13 21:44:59 +0800

    more stuff

--- js/script.js
+++ js/script.js
@@ -64,9 +64,9 @@ $(document).ready(function() {
           var to_pop = "#" + $this.data('name');
           var $to_pop = $(to_pop)
           $this.hover(function(){
-            $to_pop.fadeIn(400);
+            $to_pop.fadeIn(300);
           }, function() {
-            $to_pop.fadeOut(400)
+            $to_pop.fadeOut(300)
           })
         })
       })
