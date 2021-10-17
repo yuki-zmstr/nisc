d73ed5c6d8b8ae31bd2154738a4a38da6111b9f8
Yukihide Takahashi <yukihide99@gmail.com>
2021-10-17 18:17:18 +0800

    more stuff

--- js/script.js
+++ js/script.js
@@ -1,38 +1,37 @@
 $(document).ready(function() {
 
 
-    // Intro
-    // $(function() {
-    //     $("body, html").css({ 'overflow': "hidden", height: '100%' })
-    //     setTimeout(function() {
-    //         $("body, html").css({ overflow: 'auto', 'overflow-x': 'hidden', height: 'auto' })
-    //     }, 8500)
+    $(function() {
+        $("body, html").css({ 'overflow': "hidden", height: '100%' })
+        setTimeout(function() {
+            $("body, html").css({ overflow: 'auto', 'overflow-x': 'hidden', height: 'auto' })
+        }, 8500)
 
-    //     $("#catch")
-    //     .css({"background-image": "linear-gradient(to bottom, rgb(255, 255, 255), rgb(0, 0, 255, 0.2))"})
-    //     .delay(6500)
-    //     .fadeOut(1500)
+        $("#catch")
+        .css({"background-image": "linear-gradient(to bottom, rgb(255, 255, 255), rgb(0, 0, 255, 0.2))"})
+        .delay(6500)
+        .fadeOut(1500)
 
-    //     $("#line1")
-    //     .delay(1000)
-    //     .css({opacity: 0, "display": "block"})
-    //     .animate({opacity:1}, 1500);
+        $("#line1")
+        .delay(1000)
+        .css({opacity: 0, "display": "block"})
+        .animate({opacity:1}, 1500);
 
-    //     $("#line2")
-    //     .delay(2500)
-    //     .css({opacity: 0, "display": "block"})
-    //     .animate({opacity:1}, 1500);
+        $("#line2")
+        .delay(2500)
+        .css({opacity: 0, "display": "block"})
+        .animate({opacity:1}, 1500);
 
-    //     $("#line3")
-    //     .delay(4500)
-    //     .css({opacity: 0, "display": "block"})
-    //     .animate({opacity:1}, 1500);
+        $("#line3")
+        .delay(4500)
+        .css({opacity: 0, "display": "block"})
+        .animate({opacity:1}, 1500);
 
-    //     $("#main")
-    //     .delay(8500)
-    //     .css({ opacity: 0.0, visibility: "visible" })
-    //     .animate({ opacity: 1.0 }, 1000)
-    // });
+        $("#main")
+        .delay(8500)
+        .css({ opacity: 0.0, visibility: "visible" })
+        .animate({ opacity: 1.0 }, 1000)
+    });
 
     // background change
     $(window).scroll(function() {
